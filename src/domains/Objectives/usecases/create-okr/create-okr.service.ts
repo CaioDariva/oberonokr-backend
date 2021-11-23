eff73afc005bfbce0da90ac891decf4e1943281b
CaioDariva <cgmalavazidariva@gmail.com>
2021-11-23 00:56:02 -0300

    relation okr and user in create

--- src/domains/Objectives/usecases/create-okr/create-okr.service.ts
+++ src/domains/Objectives/usecases/create-okr/create-okr.service.ts
@@ -5,7 +5,9 @@ import { CreateOkrDto } from '../../dto/create-okr.dto';
 @Injectable()
 export class CreateOkrService {
   constructor(private readonly prisma: PrismaService) {}
-  public async execute(request: CreateOkrDto) {
-    return await this.prisma.objective.create({ data: request });
+  public async execute({ userId, ...rest }: CreateOkrDto) {
+    return await this.prisma.objective.create({
+      data: { ...rest, user: { connect: { id: userId } } },
+    });
   }
 }
